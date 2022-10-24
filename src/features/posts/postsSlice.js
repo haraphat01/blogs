756ea73e49ae4a1ed342b305c9b11320dc2b4669
haraphat01 <haraphat@gmail.com>
2022-10-24 15:24:47 +0100

    Added redux

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = [
+    {
+        id: '1',
+        title: 'Learning Redux Toolkit',
+        content: "I've heard good things.",
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+    },
+    {
+        id: '2',
+        title: 'Slices...',
+        content: "The more I say slice, the more I want pizza.",
+        reactions: {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+    }
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+
+    }
+})
+export default postsSlice.reducer
